e04ae5379096508dfb2370dc03882e91d88050f4
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-11 15:18:29 +0100

    css style and components

--- feedBackPage/src/App.jsx
+++ feedBackPage/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FeedBack from './components/FeedBack'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Feed Back Page using React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <FeedBack />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h4 className="read-the-docs">
+          Check github repo to see the code!
+        </h4>
+        <a href='https://github.com/jonriber/feed-back-page'>Here!</a>
+      </div>
+      
+      <footer><strong>Jonatas Ribeiro</strong>. 2023 #</footer>
     </>
   )
 }
